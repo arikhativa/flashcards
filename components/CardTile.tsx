a843b05c991dc4edf5b7df60cffb9df6eaad89b1
Yoav Rabby <yoavrbb@gmail.com>
2024-11-07 15:24:59 +0100

    added basic logic for multi select

--- components/CardTile.tsx
+++ components/CardTile.tsx
@@ -6,6 +6,14 @@ import { color, container, margin } from "@/constants/styles";
 import { Link } from "expo-router";
 import { Pressable, View } from "react-native";
 import { getCardHref } from "@/utils/links";
+import {
+  GestureHandlerRootView,
+  HandlerStateChangeEvent,
+  LongPressGestureHandler,
+  State,
+  TapGestureHandlerEventPayload,
+} from "react-native-gesture-handler";
+import { useState } from "react";
 
 export type CardTileProps = {
   card: Card;
@@ -14,35 +22,56 @@ export type CardTileProps = {
 };
 
 export function CardTile({ card, disabledLink, onClose }: CardTileProps) {
-  return (
-    <Link disabled={disabledLink} href={getCardHref(card.id)} asChild>
-      <Pressable>
-        <View>
-          {onClose && (
-            <IconButton
-              style={{
-                padding: 0,
-                position: "absolute",
-                top: -5,
-                right: -10,
-                zIndex: 5,
-              }}
-              icon="close"
-              size={15}
-              mode="contained"
-              onPress={onClose}
-            ></IconButton>
-          )}
+  const [isSelected, setIsSelected] = useState(false);
 
-          <PaperCard style={margin.base}>
-            <PaperCard.Content>
-              <Text variant="titleSmall">{card.sideA}</Text>
-              <Divider></Divider>
-              <Text variant="titleSmall">{card.sideB}</Text>
-            </PaperCard.Content>
-          </PaperCard>
-        </View>
-      </Pressable>
-    </Link>
+  const handleLongPress = (
+    event: HandlerStateChangeEvent<TapGestureHandlerEventPayload>
+  ) => {
+    if (event.nativeEvent.state === State.ACTIVE) {
+      setIsSelected(!isSelected);
+    }
+  };
+
+  const getSelectedStyle = () => {
+    return isSelected ? color.bgPink : {}; // TODO better style
+  };
+
+  return (
+    <GestureHandlerRootView style={{ backgroundColor: "transparent" }}>
+      <LongPressGestureHandler
+        onHandlerStateChange={handleLongPress}
+        minDurationMs={800}
+      >
+        <Link disabled={disabledLink} href={getCardHref(card.id)} asChild>
+          <Pressable>
+            <View>
+              {onClose && (
+                <IconButton
+                  style={{
+                    padding: 0,
+                    position: "absolute",
+                    top: -5,
+                    right: -10,
+                    zIndex: 5,
+                  }}
+                  icon="close"
+                  size={15}
+                  mode="contained"
+                  onPress={onClose}
+                ></IconButton>
+              )}
+
+              <PaperCard style={[margin.base, getSelectedStyle()]}>
+                <PaperCard.Content>
+                  <Text variant="titleSmall">{card.sideA}</Text>
+                  <Divider></Divider>
+                  <Text variant="titleSmall">{card.sideB}</Text>
+                </PaperCard.Content>
+              </PaperCard>
+            </View>
+          </Pressable>
+        </Link>
+      </LongPressGestureHandler>
+    </GestureHandlerRootView>
   );
 }
