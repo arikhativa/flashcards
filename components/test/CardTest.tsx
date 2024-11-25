e589be5cff00b470794c154a164da637fffea4fb
Yoav Rabby <yoavrbb@gmail.com>
2024-11-25 15:17:29 +0100

    TestCardAutoFocus

--- components/test/CardTest.tsx
+++ components/test/CardTest.tsx
@@ -1,16 +1,15 @@
-import { Dimensions, StyleSheet, View } from "react-native";
-import {
-  Text,
-  Card as PaperCard,
-  IconButton,
-  TextInput,
-} from "react-native-paper";
+import { Dimensions, View } from "react-native";
+import { Text, IconButton, TextInput } from "react-native-paper";
 import CardSides from "../shared/CardSides";
 import { Card } from "@/types/Card";
 import { margin } from "@/constants/styles";
 import { CardMeta } from "@/types/TestSettings";
-import { useState } from "react";
+import { forwardRef, useImperativeHandle, useRef, useState } from "react";
 import { useStore } from "@/providers/GlobalStore";
+
+export interface CardTestRef {
+  focusOnTextInput: () => void;
+}
 
 interface CardTestProps {
   hideSideA?: boolean;
@@ -23,111 +22,131 @@ interface CardTestProps {
   length: number;
 }
 
-export default function CardTest({
-  card,
-  hideSideA,
-  hideSideB,
-  index,
-  length,
-  cardMeta,
-  onChangeAnswer,
-  onChangeSuccess,
-}: CardTestProps) {
-  const [showAnswer, setShowAnswer] = useState(false);
-  const [showBtnColor, setShowBtnColor] = useState<boolean | undefined>(
-    undefined
-  );
+const CardTest = forwardRef<CardTestRef, CardTestProps>(
+  (
+    {
+      card,
+      hideSideA,
+      hideSideB,
+      index,
+      length,
+      cardMeta,
+      onChangeAnswer,
+      onChangeSuccess,
+    },
+    ref
+  ) => {
+    const [showAnswer, setShowAnswer] = useState(false);
+    const [showBtnColor, setShowBtnColor] = useState<boolean | undefined>(
+      undefined
+    );
 
-  const { keyboardHeight } = useStore();
-  const containerHeight = Dimensions.get("window").height - keyboardHeight;
-  const cardHeight = containerHeight / 4;
-  const paddingHeight = containerHeight / 60;
+    const { keyboardHeight } = useStore();
+    const containerHeight = Dimensions.get("window").height - keyboardHeight;
+    const cardHeight = containerHeight / 4;
+    const paddingHeight = containerHeight / 60;
 
-  return (
-    <View style={{ height: containerHeight }}>
-      <Text
-        variant="titleLarge"
-        style={{ alignSelf: "center", paddingVertical: paddingHeight }}
-      >
-        {index + 1}/{length}
-      </Text>
-      <CardSides
-        disabled
-        borderSize={20}
-        cardHeight={cardHeight}
-        style={margin.x2}
-        knowledgeLevel={card.knowledgeLevel}
-        sideA={card.sideA}
-        sideB={card.sideB}
-        hideSideA={showAnswer ? false : hideSideA}
-        hideSideB={showAnswer ? false : hideSideB}
-      />
-      <View
-        style={[
-          margin.base2,
-          {
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-          },
-        ]}
-      >
-        <TextInput
-          style={[{ flex: 1 }, margin.right]}
-          value={cardMeta.answer}
-          onChangeText={(v: string) => {
-            onChangeAnswer(index, v);
-          }}
-          label="Answer"
-        ></TextInput>
-        <IconButton
-          mode="contained"
-          icon="eye-outline"
-          onPress={() => setShowAnswer(true)}
-        ></IconButton>
-      </View>
-      <View
-        style={[
-          margin.x2,
-          {
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-          },
-        ]}
-      >
-        <Text variant="titleMedium">Did you get it right?</Text>
+    const inputRef = useRef(null);
+
+    useImperativeHandle(ref, () => ({
+      focusOnTextInput: () => {
+        if (inputRef.current) {
+          (inputRef.current as any).focus();
+        }
+      },
+    }));
+
+    return (
+      <View style={{ height: containerHeight }}>
+        <Text
+          variant="titleLarge"
+          style={{ alignSelf: "center", paddingVertical: paddingHeight }}
+        >
+          {index + 1}/{length}
+        </Text>
+        <CardSides
+          disabled
+          borderSize={20}
+          cardHeight={cardHeight}
+          style={margin.x2}
+          knowledgeLevel={card.knowledgeLevel}
+          sideA={card.sideA}
+          sideB={card.sideB}
+          hideSideA={showAnswer ? false : hideSideA}
+          hideSideB={showAnswer ? false : hideSideB}
+        />
         <View
           style={[
+            margin.base2,
             {
               flexDirection: "row",
-              justifyContent: "center",
+              justifyContent: "space-between",
               alignItems: "center",
             },
           ]}
         >
-          <IconButton
-            selected={showBtnColor && !cardMeta.success}
-            disabled={!showAnswer}
-            mode="contained"
-            icon="close"
-            onPress={() => {
-              onChangeSuccess(index, false);
-              setShowBtnColor(true);
+          <TextInput
+            ref={inputRef}
+            style={[{ flex: 1 }, margin.right]}
+            value={cardMeta.answer}
+            onChangeText={(v: string) => {
+              onChangeAnswer(index, v);
             }}
-          ></IconButton>
+            label="Answer"
+          ></TextInput>
           <IconButton
-            selected={showBtnColor && cardMeta.success}
-            disabled={!showAnswer}
             mode="contained"
-            icon="check"
-            onPress={() => {
-              onChangeSuccess(index, true);
-              setShowBtnColor(true);
-            }}
+            icon="eye-outline"
+            onPress={() => setShowAnswer(true)}
           ></IconButton>
         </View>
+        <View
+          style={[
+            margin.x2,
+            {
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            },
+          ]}
+        >
+          <Text variant="titleMedium">Did you get it right?</Text>
+          <View
+            style={[
+              {
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+              },
+            ]}
+          >
+            <IconButton
+              selected={showBtnColor && !cardMeta.success}
+              disabled={!showAnswer}
+              mode="contained"
+              icon="close"
+              onPress={() => {
+                onChangeSuccess(index, false);
+                setShowBtnColor(true);
+              }}
+            ></IconButton>
+            <IconButton
+              selected={showBtnColor && cardMeta.success}
+              disabled={!showAnswer}
+              mode="contained"
+              icon="check"
+              onPress={() => {
+                onChangeSuccess(index, true);
+                setShowBtnColor(true);
+              }}
+            ></IconButton>
+          </View>
+        </View>
       </View>
-    </View>
-  );
-}
+    );
+  }
+);
+
+CardTest.displayName = "CardTest";
+
+export default CardTest;
