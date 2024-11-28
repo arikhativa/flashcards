75f1f7a5df2c3a68042cd04f16443baf6266b260
Yoav Rabby <yoavrbb@gmail.com>
2024-11-28 16:45:47 +0100

    removing console.log on prod

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
+  const plugins = ['react-native-reanimated/plugin'];
+
+  if (process.env.NODE_ENV !== 'development') {
+    plugins.push('babel-plugin-transform-remove-console');
+  }
+
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      'react-native-reanimated/plugin',
-    ],
+    plugins:plugins,
   };
 };
