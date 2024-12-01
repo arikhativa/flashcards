27161a9d9954b228de49cddc577e9f5d02da46fd
Yoav Rabby <yoavrbb@gmail.com>
2024-12-01 09:13:17 +0100

    WhyDidYouRender

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,27 @@
 module.exports = function (api) {
   api.cache(true);
+  const presets = [];
   const plugins = ['react-native-reanimated/plugin'];
 
   if (process.env.NODE_ENV !== 'development') {
     plugins.push('babel-plugin-transform-remove-console');
   }
+  // if (process.env.NODE_ENV === 'development') {
+  //   presets.push({
+  //     jsxImportSource: "@welldone-software/why-did-you-render",
+  //   });
+  // }
 
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo',
+      [
+        '@babel/preset-react',
+        {
+          importSource: '@welldone-software/why-did-you-render',
+          development: process.env.NODE_ENV === 'development' || false,
+        },
+      ],
+    ],
     plugins:plugins,
   };
 };
