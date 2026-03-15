88c090d2ad6bf8a11382d68b6b2d18e66aa3e25d
arikhativa <arikhativa@gmail.com>
2026-03-15 18:29:50 +0200

    basic seed works

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,15 +1,40 @@
 import MainScreen from '@/components/MainScreen';
 import SettingsForm from '@/components/settings/SettingsForm';
+import { Button } from '@/components/ui/button';
+import { Typography } from '@/components/ui/text';
+import { seed } from '@/db/seed';
 import { useSuspenseConfig } from '@/hooks/query/useConfig';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Tab() {
   const { data } = useSuspenseConfig();
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: async () => {
+      console.log('🌱 Seeding database...');
+
+      return seed();
+    },
+    onSuccess: (data) => {
+      console.log('✅ Seed complete');
+      queryClient.invalidateQueries({
+        queryKey: [],
+      });
+    },
+    onError: (e) => {
+      console.error('❌ Seed failed:', e);
+    },
+  });
 
   return (
     <SafeAreaView className="flex-1">
       <MainScreen className="flex flex-col gap-6">
         <SettingsForm conf={data} />
+        <Button variant={'destructive'}>
+          <Typography onPress={() => mutate()}>Seed DB</Typography>
+        </Button>
       </MainScreen>
     </SafeAreaView>
   );
--- db/seed.ts
+++ db/seed.ts
@@ -1,10 +1,7 @@
 import { cardTable, tagTable, cardTagTable, configTable } from '@/db/schema';
 import { db } from '@/lib/db';
 
-async function seed() {
-  console.log('🌱 Seeding database...');
-
-  // 1. Clear existing data (optional, in dependency order)
+export async function seed() {
   // eslint-disable-next-line drizzle/enforce-delete-with-where
   await db.delete(cardTagTable);
   // eslint-disable-next-line drizzle/enforce-delete-with-where
@@ -31,18 +28,11 @@ async function seed() {
       {
         sideA: 'Hey',
         sideB: 'Bone Jour',
-        comment: "literary meens 'good day'",
+        comment: "literary means 'good day'",
         knowledgeLevel: 'Learning',
       },
     ])
     .returning();
 
   await db.insert(cardTagTable).values([{ cardId: card1.id, tagId: tag1.id }]);
-
-  console.log('✅ Seed complete');
 }
-
-seed().catch((err) => {
-  console.error('❌ Seed failed:', err);
-  process.exit(1);
-});
