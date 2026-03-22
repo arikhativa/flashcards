755ed8af8144c3e1817450f46776887e9f98d92f
arikhativa <arikhativa@gmail.com>
2026-03-22 16:39:18 +0200

    better bot sheet

--- components/BottomSheetList.tsx
+++ components/BottomSheetList.tsx
@@ -1,6 +1,11 @@
 import { THEME } from '@/lib/theme';
-import BottomSheet, { BottomSheetTextInput, BottomSheetView } from '@gorhom/bottom-sheet';
-import { forwardRef } from 'react';
+import BottomSheet, {
+  BottomSheetBackdrop,
+  BottomSheetTextInput,
+  BottomSheetView,
+} from '@gorhom/bottom-sheet';
+import { useColorScheme } from 'nativewind';
+import { forwardRef, useCallback } from 'react';
 import { View } from 'react-native';
 
 interface Props {
@@ -11,19 +16,29 @@ interface Props {
 
 export const BottomSheetList = forwardRef<BottomSheet, Props>(
   ({ search, onChangeText, children }, ref) => {
+    const { colorScheme } = useColorScheme();
+
+    const renderBackdrop = useCallback(
+      (props: any) => (
+        <BottomSheetBackdrop {...props} disappearsOnIndex={-1} appearsOnIndex={0} opacity={0.3} />
+      ),
+      []
+    );
+
     return (
       <BottomSheet
         index={-1}
-        snapPoints={['90%']}
+        snapPoints={['60%']}
         ref={ref}
         enablePanDownToClose={true}
         backgroundStyle={{
-          borderTopWidth: 0.5,
-          borderWidth: 0.5,
-          borderColor: 'var(--border-color)',
-        }}>
+          borderTopWidth: 25,
+          borderWidth: 0,
+          borderColor: THEME[colorScheme || 'light'].primary,
+        }}
+        backdropComponent={renderBackdrop}>
         <BottomSheetView className="flex-1">
-          <View className="px-4 py-2">
+          <View className="px-4 pt-4">
             <BottomSheetTextInput
               placeholder="Search..."
               value={search}
